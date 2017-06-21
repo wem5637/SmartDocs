c10f4786ba20a0b8f6fe5402723d89af14098d26
Rob Tong <robtong21@gmail.com>
2017-06-21 12:27:59 -0500

    landing page/login/signup

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -1,42 +1,9 @@
 import React from 'react'
-import bootstrap from 'bootstrap'
+import { Link } from 'react-router'
 
 const Landing = () => {
   return (
     <div>
-      <nav className="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                Menu <i className="fa fa-bars"></i>
-            </button>
-            <a className="navbar-brand page-scroll" href="#page-top">
-                <i className="fa fa-play-circle"></i> <span className="light">Start</span> SmartDocs
-            </a>
-          </div>
-
-          <div className="collapse navbar-collapse navbar-right navbar-main-collapse">
-            <ul className="nav navbar-nav">
-              <li className="hidden">
-                  <a href="#page-top"></a>
-              </li>
-              <li>
-                  <a className="page-scroll" href="#about">About</a>
-              </li>
-              <li>
-                  <a className="page-scroll" href="#download">Login</a>
-              </li>
-              <li>
-                  <a className="page-scroll" href="#download">Sign Up</a>
-              </li>
-              <li>
-                  <a className="page-scroll" href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
       <header className="intro">
         <div className="intro-body">
           <div className="container">
--- app/main.jsx
+++ app/main.jsx
@@ -1,6 +1,6 @@
 'use strict'
 import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {Router, Route, Link, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
@@ -10,6 +10,7 @@ import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import Landing from './components/Landing'
 import NotFound from './components/NotFound'
+import SignupContainer from './containers/SignupContainer'
 import EditorContainer from './containers/EditorContainer'
 
 const ExampleApp = connect(
@@ -17,8 +18,41 @@ const ExampleApp = connect(
 )(
   ({ user, children }) =>
     <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
+      <nav className="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                Menu <i className="fa fa-bars"></i>
+            </button>
+            <a className="navbar-brand page-scroll" href="#page-top">
+                <i className="fa fa-play-circle"></i> <span className="light">Start</span> SmartDocs
+            </a>
+          </div>
+
+          <div className="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul className="nav navbar-nav">
+              <li className="hidden">
+                  <a href="#page-top"></a>
+              </li>
+              <li>
+                  <Link to='/about' className="page-scroll">About</Link>
+              </li>
+              {user ? <WhoAmI/>
+              : <span>
+                  <li>
+                    <Link to='/login' className="page-scroll">Login</Link>
+                  </li>
+                  <li>
+                      <Link to='/signup' className="page-scroll">Sign Up</Link>
+                  </li>
+                </span>
+              }
+                <li>
+                    <Link to='/contact' className="page-scroll">Contact</Link>
+                </li>
+            </ul>
+          </div>
+        </div>
       </nav>
       {children}
     </div>
@@ -29,6 +63,8 @@ render(
     <Router history={browserHistory}>
       <Route path="/" component={ExampleApp}>
         <IndexRedirect to="/landing" />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={SignupContainer} />
         <Route path="/landing" component={Landing} />
         <Route path="/editor" component={EditorContainer} />
       </Route>
